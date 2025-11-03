a65537163f05ec625b47978b9e4b100a3e8638b9
Agustin <agustindelossantos023@gmail.com>
2025-11-03 22:37:47 +0100

    ADD animated-arrow-c

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeP } from './pages/home-p/home-p';
 import { BricksP } from './pages/bricks-p/bricks-p';
+import { ArrowsP } from './pages/arrows-p/arrows-p';
 
 export const routes: Routes = [
   {
@@ -12,6 +13,11 @@ export const routes: Routes = [
     path: 'bricks',
     component: BricksP,
     title: 'Bricks'
+  },
+  {
+    path: 'arrows',
+    component: ArrowsP,
+    title: 'Arrows'
   },
   {
     path: '**',
